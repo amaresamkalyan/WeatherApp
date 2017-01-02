26c834c2c50b3d2bb3316f06e1888d79d2c76cef
amaresamkalyan <amaresamkalyan@gmail.com>
2017-01-01 21:27:52 -0400

    .

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 
 var Examples = (propd) => {
-    return (<h3>Examples  Component </h3>)
+    return (<h3>Examples  - omponent </h3>)
 };
 
 module.exports = Examples;
