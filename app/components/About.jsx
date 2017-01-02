35dcba91a530bad8878b729d6a81747d4c7d4190
amaresamkalyan <amaresamkalyan@gmail.com>
2017-01-02 17:29:05 -0400

    .

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,22 +4,23 @@ var About = (props) => {
     return (
         <div>
             <h1 className="text-center">About</h1>
-            <h4 className="text-center">My name is Kalyan Amaresam. </h4>
+
+            <h4 className="text-center">My name is Kalyan Amaresam.</h4>
+
             <p>This is a weather application build on React. I have built this
-        for The Complete React Web App Developer Course.
-      </p>
-            <p>
-                Here are some of the tools I used:
-      </p>
+               for The Complete React Web App Developer Course.</p>
+
+            <p>Here are some of the tools I used:</p>
+
             <ul>
                 <li>
-                    <a href="https://facebook.github.io/react">React</a> - This was the
-            JavaScript framework used.
-        </li>
+                    <a href="https://facebook.github.io/react">React </a> 
+                    - This was the JavaScript framework used.
+                </li>
                 <li>
-                    <a href="http://openweathermap.org">Open Weather Map</a> - I used
-            Open Weather Map to search for weather data by city name.
-        </li>
+                    <a href="http://openweathermap.org">Open Weather Map </a> 
+                    - I used Open Weather Map to search for weather data by city name.
+                </li>
             </ul>
         </div>
     )
