824318a94617a397daaf214be8f18b20cdd97799
amaresamkalyan <amaresamkalyan@gmail.com>
2017-01-03 23:42:08 -0400

    .

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-            <h1 className="text-center">About</h1>
+            <h1 className="text-center page-title">About</h1>
 
             <h4 className="text-center">My name is Kalyan Amaresam.</h4>
 
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -27,7 +27,7 @@ var Nav = React.createClass({
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
-                <input type="search" placeholder="Enter City" />
+                <input type="search" placeholder="Enter City Name" />
               </li>
               <li>
                 <input type="submit" className="button" value="Submit" />
